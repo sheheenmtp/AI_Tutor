a02ae550f803b4a481bc2572964a0c461d2ef902
sheheenmtp <muhammadsheheen@gmail.com>
2026-02-26 15:27:18 +0530

    Stop tracking local SQL files

--- user_auth_migration.sql
+++
@@ -1,9 +0,0 @@
--- Add authentication fields to existing users table.
--- Run once on your live database.
-
-BEGIN;
-
-ALTER TABLE public.users
-ADD COLUMN IF NOT EXISTS password_hash character varying(255);
-
-COMMIT;
